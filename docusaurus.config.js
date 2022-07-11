b3b2c4ab6035d93e056accd52b834c0cbb3a8b28
Jody Heavener <j.heavener@gmail.com>
2022-07-11 16:01:14 -0300

    Use trailingSlash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'jodyheavener', // Usually your GitHub org/user name.
   projectName: 'docusaurus-playground', // Usually your repo name.
+  trailingSlash: true,
 
   presets: [
     [
